f7e73be3be76c2f81a475f782de10b56f86312f7
zzwhjj <zzwhjj@126.com>
2020-06-27 20:40:28 +0800

    改为Maven

---
+++ src/main/java/com/tank/ResourceMgr.java
@@ -0,0 +1,21 @@
+package com.tank;
+
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+//资源管理类
+public class ResourceMgr {
+
+    public static BufferedImage tankL, tankU, tankR, tankD;
+    static {
+        try {
+            tankL = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankL.gif"));
+            tankU = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankU.gif"));
+            tankR = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankR.gif"));
+            tankD = ImageIO.read(ResourceMgr.class.getClassLoader().getResourceAsStream("images/tankD.gif"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/tank/Tank.java
@@ -0,0 +1,90 @@
+package com.tank;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Tank {
+
+    private int x, y;
+    private Dir dir = Dir.DOWN;
+    final int SPEED = 5;
+    private boolean moving = false;
+
+    private TankFrame tf = null;
+
+    public Tank(int x, int y, Dir dir, TankFrame tf) {
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.tf = tf;
+    }
+
+    public void paint(Graphics graphics) {
+        graphics.drawImage(ResourceMgr.tankL, x, y, null);
+
+        move();
+    }
+
+    private void move() {
+        if (!moving) {
+            return;
+        }
+
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void fire() {
+        tf.bullets.add(new Bullet(this.x, this.y, this.dir, tf));
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Dir getDir() {
+        return dir;
+    }
+
+    public void setDir(Dir dir) {
+        this.dir = dir;
+    }
+
+    public int getSPEED() {
+        return SPEED;
+    }
+
+    public boolean isMoving() {
+        return moving;
+    }
+
+    public void setMoving(boolean moving) {
+        this.moving = moving;
+    }
+}
