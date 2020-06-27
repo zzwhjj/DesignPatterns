102dc86b3863b78697736f6db6adf8ef7a17f957
zzwhjj <zzwhjj@126.com>
2020-06-27 12:04:51 +0800

    键盘控制坦克方向

---
+++ src/com/tank/Dir.java
@@ -0,0 +1,5 @@
+package com.tank;
+
+public enum Dir {
+    LEFT, UP, RIGHT, DOWN
+}
--- src/com/tank/TankFrame.java
+++ src/com/tank/TankFrame.java
@@ -8,8 +8,11 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame {
+
     int x = 200;
     int y = 200;
+    Dir dir = Dir.DOWN;
+    final int SPEED = 10;
 
     public TankFrame() {
         setSize(800, 600);
@@ -30,38 +33,94 @@ public class TankFrame extends Frame {
     @Override
     public void paint(Graphics graphics) {
         graphics.fillRect(x, y, 50, 50);
+
+        switch (dir) {
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            default:
+                break;
+        }
         //x += 10;
         //y += 10;
     }
 
     class MyKeyListener extends KeyAdapter {
 
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
             int key = e.getKeyCode();
             switch (key) {
                 case KeyEvent.VK_LEFT:
-                    x -= 10;
+                    bL = true;
                     break;
                 case KeyEvent.VK_UP:
-                    y -= 10;
+                    bU = true;
                     break;
                 case KeyEvent.VK_RIGHT:
-                    x += 10;
+                    bR = true;
                     break;
                 case KeyEvent.VK_DOWN:
-                    y += 10;
+                    bD = true;
                     break;
                 default:
                     break;
             }
             //x += 200;
-           // repaint();
+            // repaint();
+            setMainTankDir();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
+            int key = e.getKeyCode();
+            switch (key) {
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
 
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if (bL) {
+                dir = Dir.LEFT;
+            }
+            if (bU) {
+                dir = Dir.UP;
+            }
+            if (bR) {
+                dir = Dir.RIGHT;
+            }
+            if (bD) {
+                dir = Dir.DOWN;
+            }
         }
     }
 }
