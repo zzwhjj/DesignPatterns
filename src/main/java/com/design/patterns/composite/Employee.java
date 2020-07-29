de24c2b41f1ebfa8776d213f273eeded53582bc8
zzwhjj <zzwhjj@126.com>
2020-07-29 21:19:30 +0800

    组合模式

---
+++ src/main/java/com/design/patterns/composite/Employee.java
@@ -0,0 +1,43 @@
+package com.design.patterns.composite;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 创建Employee类，该类带有Employee对象的列表
+ */
+public class Employee {
+
+    private String name;
+    private String dept;
+    private int salary;
+    private List<Employee> subordinates;
+
+    public Employee(String name, String dept, int salary) {
+        this.name = name;
+        this.dept = dept;
+        this.salary = salary;
+        subordinates = new ArrayList<>();
+    }
+
+    public void add(Employee e) {
+        subordinates.add(e);
+    }
+
+    public void remove(Employee e) {
+        subordinates.remove(e);
+    }
+
+    public List<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+            "name='" + name + '\'' +
+            ", dept='" + dept + '\'' +
+            ", salary=" + salary +
+            '}';
+    }
+}
