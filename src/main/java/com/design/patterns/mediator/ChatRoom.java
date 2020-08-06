ca10ae9dd4b1735b5b1770df9e32c18aab4cf32e
zzwhjj <zzwhjj@126.com>
2020-08-06 23:56:19 +0800

    中介者模式

---
+++ src/main/java/com/design/patterns/mediator/ChatRoom.java
@@ -0,0 +1,13 @@
+package com.design.patterns.mediator;
+
+import java.util.Date;
+
+/**
+ * 中介类
+ */
+public class ChatRoom {
+
+    public static void showMessage(User user, String message) {
+        System.out.println(new Date().toString() + " [" + user.getName() + "]: " + message);
+    }
+}
