3f61fbbdf146db912d437238ce9694a558c012dd
zzwhjj <zzwhjj@126.com>
2020-08-11 22:16:17 +0800

    空对象模式

---
+++ src/main/java/com/design/patterns/nullobject/AbstractCustomer.java
@@ -0,0 +1,8 @@
+package com.design.patterns.nullobject;
+
+public abstract class AbstractCustomer {
+
+    protected String name;
+    public abstract boolean isNil();
+    public abstract String getName();
+}
---
+++ src/main/java/com/design/patterns/nullobject/NullCustomer.java
@@ -0,0 +1,14 @@
+package com.design.patterns.nullobject;
+
+public class NullCustomer extends AbstractCustomer {
+
+    @Override
+    public boolean isNil() {
+        return true;
+    }
+
+    @Override
+    public String getName() {
+        return "Not Available in Customer Database";
+    }
+}
